057a83a4323246f7e43b2d073e7e494b18a5f079
vita792 <59779169+vita792@users.noreply.github.com>
2020-06-24 01:24:57 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-package com.company.lesson7;
+package com.company.lesson6;
 
 import java.util.Scanner;
 
@@ -13,19 +13,19 @@ public class Main {
         } else if (count == 2) {
             System.out.println("#\n##");
         } else if (count == 3) {
-            System.out.println("###\n##\n#");
+            System.out.println("#\n##\n###");
         } else if (count == 4) {
-            System.out.println("####\n###\n##\n#");
+            System.out.println("#\n##\n###\n####");
         } else if (count == 5) {
-            System.out.println("#####\n####\n###\n##\n#");
+            System.out.println("#\n##\n###\n####\n#####\n#####");
         } else if (count == 6) {
-            System.out.println("######\n#####\n####\n###\n##\n#");
+            System.out.println("#\n##\n###\n####\n#####\n######\n######");
         } else if (count == 7) {
-            System.out.println("#######\n######\n#####\n####\n###\n##\n#");
+            System.out.println("#\n##\n###\n####\n#####\n######\n#######");
         } else if (count == 8) {
-            System.out.println("########\n#######\n######\n#####\n####\n###\n##\n#");
+            System.out.println("#\n##\n###\n####\n#####\n######\n#######\n########");
         } else if (count == 9) {
-            System.out.println("##########\n########\n#######\n######\n#####\n####\n###\n##\n#");
+            System.out.println("#\n##\n###\n####\n#####\n######\n#######\n########\n##########");
         } else {
             System.out.println("Please, enter number from 1 to 9!");
         }
