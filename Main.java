c8aeecff9e55d33dbe10124c3208f88786c6ec2e
vita792 <59779169+vita792@users.noreply.github.com>
2020-06-24 18:34:47 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-package com.company.lesson2;
+package com.company.lesson3;
 
         import java.util.Scanner;
 
@@ -9,32 +9,22 @@ public class Main {
         int[] userNumbers = new int[3];
         System.out.println("Enter three numbers: ");
         Scanner numb = new Scanner(System.in);
-        for (int i =0; i < userNumbers.length; i++) {
+
+        for (int i = 0; i < userNumbers.length; i++) {
             userNumbers[i] = numb.nextInt();
+
         }
-        if        (userNumbers[0]%2 != 0 && userNumbers[1]%2 != 0  && userNumbers[2]%2 != 0) {
-            System.out.println("Your summ is: " + (userNumbers[0] + userNumbers[1] + userNumbers[2]));
+            System.out.println("The MAXelement is: " +
+                    Math.max(
+                            Math.max(userNumbers[0], userNumbers[1]),
+                            Math.max(userNumbers[1], userNumbers[2])
+                              ));
 
-        } else if (userNumbers[0]%2 == 0 && userNumbers[1]%2 != 0  && userNumbers[2]%2 != 0) {
-            System.out.println("Your summ is: " + (userNumbers[1] + userNumbers[2]));
+            System.out.println("The MIXelement is: " +
+                Math.min(
+                        Math.min(userNumbers[0], userNumbers[1]),
+                        Math.min(userNumbers[1], userNumbers[2])
+                ));
 
-        } else if (userNumbers[0]%2 == 0 && userNumbers[1]%2 == 0 && userNumbers[2]%2 != 0) {
-            System.out.println("Your summ is: " + (userNumbers[2]));
-
-        } else if (userNumbers[0]%2 == 0 && userNumbers[1]%2 != 0 && userNumbers[2]%2 == 0) {
-            System.out.println("Your summ is: " + (userNumbers[1]));
-
-        } else if (userNumbers[0]%2 != 0 && userNumbers[1]%2 == 0 && userNumbers[2]%2 != 0) {
-            System.out.println("Your summ is: " + (userNumbers[0] + userNumbers[2]));
-
-        } else if (userNumbers[0]%2 != 0 && userNumbers[1]%2 == 0 && userNumbers[2]%2 == 0) {
-            System.out.println("Your summ is: " + (userNumbers[0]));
-
-        } else if (userNumbers[0]%2 != 0 && userNumbers[1]%2 != 0  && userNumbers[2]%2 == 0) {
-            System.out.println("Your summ is: " + (userNumbers[0] + userNumbers[1]));
-
-        } else {
-            System.out.println("There are no odd numbers!" );
-        }
     }
 }
